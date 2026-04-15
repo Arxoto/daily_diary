673ba7d767d63e8d4704bd1de7ad188f3d61d277
Arxoto <1484663586@qq.com>
2026-04-15 22:54:05 +0800

    更新 music 导出脚本

--- music/0_show_me_the_power.sh
+++ music/0_show_me_the_power.sh
@@ -1,2 +1,7 @@
-cat all.csv | sed 1d | awk -F'","' '{ print $1," - ",$2}' | tr -d '"' | grep -v 'Landing Guy'
+cat all.csv | sed 1d | awk -F'","' '{ print $1," - ",$2}' | tr -d '"' \
+| grep -v 'Landing Guy' \
+| grep -v '女儿情' \
+| grep -v '女兒情'
+
 echo 'Landing Guy  -  刘昊霖'
+echo '女儿情  -  吴静'
